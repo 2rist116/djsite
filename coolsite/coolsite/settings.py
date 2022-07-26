22dc569fc137406594d0fd769b1c65c5dceeede2
Vitaliy Inyushev <vinyushev@mail.ru>
2022-07-26 20:44:23 +0300

    маршрутизация и исключения

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!3z1i0jos^guv*=q&9uuvi2c3=b*o$y_wh4mte4m1outo4fy_5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
