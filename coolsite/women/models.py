172f7fabe32c6953505ede5511bd237835da8621
Vitaliy Iniushev <vinyushev@mail.ru>
2022-07-29 13:13:00 +0300

    закончил 11 урок

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -2,25 +2,31 @@ from django.db import models
 from django.urls import reverse
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat  = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+    cat  = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
 
     def __str__(self):
         return self.title
 
-
+    # дает ссылку на пост по pk и добавляет кнопку посмотреть на сайте в админке
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    # Замена названий полей в адмике, сортировка 
+    class Meta:
+        verbose_name = 'Известные женщины'
+        verbose_name_plural = 'Известные женщины'
+        ordering = ['time_create', 'title']
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     
     def __str__(self):
@@ -29,3 +35,9 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
